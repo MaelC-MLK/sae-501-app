eb44ee18cf8f108bd300ea0846cc72b44f64ce40
Robin Hajnoczy <101249228+Robinho0413@users.noreply.github.com>
2024-10-15 12:56:13 +0000

    variables tw

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="outline">Button</Button>
+      <Button variant="default">default</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="link">link</Button>
     </div>
   );
 }
