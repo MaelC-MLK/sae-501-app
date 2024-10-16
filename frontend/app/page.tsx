1cb77824c61344225bf9ced94800998ffe3cb4cb
Robin Hajnoczy <101249228+Robinho0413@users.noreply.github.com>
2024-10-16 07:24:20 +0000

    Card component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import Image from 'next/image'
 import { Badge } from "@/components/ui/badge"
 import ScrollButton from "@/components/ui/scrollButton"
+import { CardEvent } from "@/components/cards/cardEvent"
 import {
   Carousel,
   CarouselContent,
@@ -51,29 +52,7 @@ export default function Home() {
           {events.map((event, index) => (
               <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
                 <div className="p-1">
-                  <Card className="overflow-hidden relative cursor-pointer shadow">
-                    <div className="relative w-full h-64">
-                      <Image
-                        src={event.image}
-                        alt={`${event.title}-image`}
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                      <div className="group absolute -bottom-28 left-0 flex flex-col text-secondary bg-black bg-opacity-70 rounded-lg w-full hover:-translate-y-28 transition-transform duration-300">
-                        <CardHeader className="flex flex-row justify-between">
-                          <div>
-                            <CardTitle className="text-2xl">{event.title}</CardTitle>
-                            <span className="text-lg font-medium">{event.date}</span>
-                          </div>
-                          <Badge variant="outline" className="h-fit">{event.badge}</Badge>
-                        </CardHeader>
-                        <CardContent className="transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                          <span className="text-md font-normal">{event.time}</span>
-                          <CardDescription className="line-clamp-4 h-20">{event.description}</CardDescription>
-                        </CardContent>
-                      </div>
-                    </div>
-                  </Card>
+                  <CardEvent event={event} />
                 </div>
               </CarouselItem>
             ))}
