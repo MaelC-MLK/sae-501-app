2679e72906c940587c0a15362b486577e100c153
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-10-24 22:53:52 +0200

    First commit

--- backend/migrations/Version20241024205125.php
+++ backend/migrations/Version20241024205125.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241024142400 extends AbstractMigration
+final class Version20241024205125 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20241024142400 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD image VARCHAR(5) DEFAULT NULL, ADD location VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user DROP birth_date, DROP avatar');
+        $this->addSql('ALTER TABLE event ADD is_draft TINYINT(1) NOT NULL, DROP state');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `user` ADD birth_date DATE NOT NULL, ADD avatar LONGBLOB DEFAULT NULL');
-        $this->addSql('ALTER TABLE event DROP image, DROP location');
+        $this->addSql('ALTER TABLE event ADD state VARCHAR(255) DEFAULT NULL, DROP is_draft');
     }
 }
