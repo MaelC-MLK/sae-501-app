d12c6d576c8518e79640d5b563a919949d67f816
TomBoutin <tomboutin.tom@gmail.com>
2024-10-24 15:18:02 +0200

    entité event

--- backend/src/Entity/Event.php
+++ backend/src/Entity/Event.php
@@ -39,6 +39,7 @@ class Event
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $state = null;
 
+
     public function __construct()
     {
         $this->users = new ArrayCollection();
@@ -144,5 +145,6 @@ class Event
 
         return $this;
     }
+
 
 }
