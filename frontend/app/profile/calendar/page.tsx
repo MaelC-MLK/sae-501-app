210d23de6b2a0f2ef5f7a0552cea791ec6fdd296
MaelC-MLK <maelcheron@gmail.com>
2024-10-24 15:24:19 +0200

    Add calendar section to profile page

--- frontend/app/profile/calendar/page.tsx
+++ frontend/app/profile/calendar/page.tsx
@@ -1,10 +1,12 @@
 import PopupCreationEvent from "@/components/sections/popupCreationEvent";
+import Calendar from "@/components/sections/calendar";
 
 export default function Page(){
     
     return (
         <>
             <PopupCreationEvent />
+            <Calendar />
         </>
     )
 }
