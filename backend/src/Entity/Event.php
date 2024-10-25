5eb65484449abe5a9d1bef241c4301a112bf84e8
TomBoutin <144112026+TomBoutin@users.noreply.github.com>
2024-10-25 21:28:15 +0200

    Update Event.php

--- backend/src/Entity/Event.php
+++ backend/src/Entity/Event.php
@@ -63,10 +63,7 @@ class Event
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $location = null;
-
-    #[ORM\ManyToOne(inversedBy: 'owned')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $creator = null;
+  
     #[ORM\Column]
     private ?bool $is_draft = null;
 
@@ -190,15 +187,6 @@ class Event
         return $this;
     }
 
-    public function getCreator(): ?User
-    {
-        return $this->creator;
-    }
-
-    public function setCreator(?User $creator): static
-    {
-        $this->creator = $creator;
-    }
     public function isIsDraft(): ?bool
     {
         return $this->is_draft;
