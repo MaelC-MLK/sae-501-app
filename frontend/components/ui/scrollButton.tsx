b5809c6669d58999e748e98f4fc372b75af80aee
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-12 09:53:08 +0100

    fix - scroll button

--- frontend/components/ui/scrollButton.tsx
+++ frontend/components/ui/scrollButton.tsx
@@ -4,14 +4,17 @@ import Image from 'next/image';
 
 const ScrollButton = () => {
     const handleScroll = () => {
-        window.scrollTo({
-            top: window.innerHeight,
-            behavior: 'smooth',
-        });
+        const element = document.getElementById('scroll-btn');
+        if (element) {
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
     };
 
     return (
-        <div onClick={handleScroll} className="w-fit flex flex-col items-center justify-center mx-auto gap-1 cursor-pointer mt-52 hover:translate-y-2 transition-transform duration-300">
+        <div id='scroll-btn' onClick={handleScroll} className="w-fit flex flex-col items-center justify-center mx-auto gap-1 cursor-pointer mt-52 hover:translate-y-2 transition-transform duration-300">
             <span className="text-sm text-primary">Voir plus d'évènements</span>
             <Image
                 src="/images/ScrollDown.svg"
