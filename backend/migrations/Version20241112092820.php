10f7f79ea70a80066d24a0102d3a347edd4e3323
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-12 11:11:59 +0100

    feat - filter recommended events

--- backend/migrations/Version20241112092820.php
+++ backend/migrations/Version20241112092820.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241024205125 extends AbstractMigration
+final class Version20241112092820 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241024205125 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD is_draft TINYINT(1) NOT NULL, DROP state');
+        $this->addSql('ALTER TABLE event ADD is_recommended TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD state VARCHAR(255) DEFAULT NULL, DROP is_draft');
+        $this->addSql('ALTER TABLE event DROP is_recommended');
     }
 }
--- frontend/components/sections/carouselRecom.tsx
+++ frontend/components/sections/carouselRecom.tsx
@@ -1,28 +1,28 @@
-import * as React from "react"
-
-import { CardEvent } from "@/components/cards/cardEvent"
-
+import * as React from "react";
+import { CardEvent } from "@/components/cards/cardEvent";
 import { EventProps } from "@/types/event";
-
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
 interface CarouselRecomProps {
     events: EventProps[];
 }
 
-
 export function CarouselRecom({ events }: CarouselRecomProps) {
+    const recommendedEvents = events.filter(event => event.isRecommended);
+
+
     console.log(events)
+    console.log(recommendedEvents)
     return (
         <Carousel className="w-full">
             <CarouselContent className="-ml-1">
-                {events.map((event, index) => (
+                {recommendedEvents.map((event, index) => (
                     <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
                         <div className="p-1">
                             <CardEvent event={event} />
@@ -33,5 +33,5 @@ export function CarouselRecom({ events }: CarouselRecomProps) {
             <CarouselPrevious className="hover:scale-125 transition-transform duration-200" />
             <CarouselNext className="hover:scale-125 transition-transform duration-200" />
         </Carousel>
-    )
+    );
 }
