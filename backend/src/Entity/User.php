47a8bddfe7c57a78251969568663139d0076ed43
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-12 12:13:52 +0100

    PlainPassword can be null

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -56,7 +56,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $password = null;
 
-    #[Assert\NotBlank(groups: ['user:create'])]
     #[Groups(['user:create', 'user:update'])]
     private ?string $plainPassword = null;
 
