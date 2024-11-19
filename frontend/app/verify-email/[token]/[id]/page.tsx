dc9e2c004b0aa514db6834c2420f278f4461b65d
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-11-19 14:37:25 +0100

    Update email verification process and add Google Mailer support

---
+++ frontend/app/verify-email/[token]/[id]/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useEffect, useState, useRef } from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function VerifyEmailPage({ params }: { params: { token: string, id: string } }) {
+    const { token, id } = params;
+    const [status, setStatus] = useState<'loading' | 'success' | 'error' | 'alreadyVerified' | null>('loading');
+    const [message, setMessage] = useState<string | null>(null);
+    const hasFetched = useRef(false); // Référentiel pour suivre l'état de la requête
+
+    useEffect(() => {
+        if (hasFetched.current) return; // Si déjà fetché, ne rien faire
+        hasFetched.current = true; // Marquer comme fetché
+
+        const verifyToken = async () => {
+            try {
+                const res = await fetch(`http://localhost:8080/api/verify-email/${token}/${id}`);
+                if (res.ok) {
+                    const data = await res.json();
+                    if (data.message === 'Utilisateur déjà inscrit à l\'événement.') {
+                        setStatus('alreadyVerified');
+                        setMessage(data.message);
+                    } else {
+                        setStatus('success');
+                        setMessage('Votre inscription a été validée avec succès !');
+                    }
+                } else {
+                    const data = await res.json();
+                    setStatus('error');
+                    setMessage(data.error || 'Token invalide.');
+                }
+            } catch (err) {
+                setStatus('error');
+                setMessage('Erreur lors de la vérification.');
+            }
+        };
+
+        verifyToken();
+    }, [token, id]);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md text-center">
+                {status === 'loading' && <h1>Vérification de l'email...</h1>}
+                {(status === 'success' || status === 'alreadyVerified' || status === 'error') && (
+                    <>
+                        <h1 className='text-lg font-semibold'>{message}</h1>
+                        <Link href={'/'}>
+                            <Button
+                                variant={'default'}
+                                size={'lg'}
+                                className="mt-4"
+                            >
+                                Retour à l'accueil
+                            </Button>
+                        </Link>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
