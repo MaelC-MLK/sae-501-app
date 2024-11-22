ed35c86fb102fc46db4d1c150d4de9bd7ebb3adb
n1noT <n8no.ttn@gmail.com>
2024-11-22 15:03:25 +0100

    qs

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -93,6 +93,7 @@ export default function Profile() {
                   <PopUpEditProfile
                     user={userData}
                     onUpdate={(updatedUser) => setUserData(updatedUser)}
+                    userContext={userContext}
                   />
                 ) : (
                   <p>Chargement du profil...</p>
