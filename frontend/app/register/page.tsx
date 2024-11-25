d64ba92669b61d9256bcf17d4cbe0540f86cb290
Mathis <mathis.micheau14@gmail.com>
2024-11-25 16:27:02 +0100

    refonte register page

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { authenticate } from "@/lib/utils"
 import Link from 'next/link';
 import { useUser } from '@/contexts/UserProvider';
+import { k2d } from "@/app/fonts/fonts"
 import { useRouter } from 'next/navigation';
 
 
@@ -96,26 +97,13 @@ export default function Page() {
 
     return (
         <div className="flex h-full">
-            <div className="flex flex-col justify-items-center lg:w-2/3 p-4 sm:p-12 lg:p-24 lg:px-56">
-                <h1 className="text-2xl font-bold mb-6">Créer un compte</h1>
-
-                <div className="mb-10">
-                    <Button className="w-full bg-blue-500 text-white py-6 rounded-md text-lg mb-3 hover:bg-blue-600 transition">
-                        S'inscrire avec Google
-                    </Button>
-                    <Button className='w-full py-6 text-lg' variant="outline">S'inscrire avec Apple</Button>
-                </div>
-
-                <div className="flex items-center justify-center mb-6">
-                    <div className="w-full border-t border-gray-300"></div>
-                    <span className="px-3 text-gray-500">or</span>
-                    <div className="w-full border-t border-gray-300"></div>
-                </div>
+            <div className="flex flex-col justify-items-center md:w-2/3 p-4 sm:p-12 lg:p-24 lg:px-56">
+                <h1 className={`${k2d.className} text-3xl font-bold mb-6`}>Rejoignez notre communauté</h1>
 
                 <form onSubmit={handleSubmit}>
 
                     <div className="mb-4">
-                        <Label className='text-lg' htmlFor="firstName">Prénom</Label>
+                        <Label className='text-lg' htmlFor="firstName">Prénom*</Label>
                         <Input
                             type="text"
                             id="firstName"
@@ -128,7 +116,7 @@ export default function Page() {
                     </div>
 
                     <div className="mb-4">
-                        <Label className='text-lg' htmlFor="lastName">Nom</Label>
+                        <Label className='text-lg' htmlFor="lastName">Nom*</Label>
                         <Input
                             type="text"
                             id="lastName"
@@ -141,11 +129,11 @@ export default function Page() {
                     </div>
 
                     <div className="mb-4">
-                        <Label className='text-lg' htmlFor="email">Email</Label>
+                        <Label className='text-lg' htmlFor="email">Adresse mail*</Label>
                         <Input
                             type="email"
                             id="email"
-                            placeholder="E-mail"
+                            placeholder="Adresse mail"
                             name="email"
                             required
                             className="mt-1 w-full p-2 border border-gray-300 rounded-md text-md h-10"
@@ -154,11 +142,11 @@ export default function Page() {
                     </div>
 
                     <div className="mb-6">
-                        <Label className='text-lg' htmlFor="plainPassword">Mot de passe</Label>
+                        <Label className='text-lg' htmlFor="plainPassword">Mot de passe*</Label>
                         <Input
                             type="password"
                             id="plainPassword"
-                            placeholder="Mot de passe"
+                            placeholder="**********"
                             name="plainPassword"
                             required
                             className="mt-1 w-full p-2 border border-gray-300 rounded-md text-md h-10"
@@ -166,11 +154,11 @@ export default function Page() {
                             onChange={(e) => setPlainPassword(e.target.value)} />
                     </div>
                     <div className="mb-4">
-                        <Label className="text-lg" htmlFor="confirmPassword">Confirmer le mot de passe</Label>
+                        <Label className="text-lg" htmlFor="confirmPassword">Confirmer le mot de passe*</Label>
                         <Input
                             type="password"
                             id="confirmPassword"
-                            placeholder="Confirmer le mot de passe"
+                            placeholder="**********"
                             name="confirmPassword"
                             required
                             className="mt-1 w-full p-2 border border-gray-300 rounded-md text-md h-10"
@@ -179,18 +167,18 @@ export default function Page() {
                         />
                     </div>
                     {error && <p className="text-red-500 text-sm">{error}</p>}
-                    <Button type="submit" className="mt-6 w-full bg-blue-500 text-white text-lg py-6 rounded-md hover:bg-blue-600 transition">
+                    <Button type="submit" className="mt-6 w-full bg-primary text-white text-lg py-6 rounded-md transition">
                         S'incrire
                     </Button>
                 </form>
 
-                <p className="text-center text-sm text-gray-500 mt-6">
-                    Vous avez déjà un compte? <Link href="/login" className="text-blue-500 hover:underline">Connectez-vous</Link>
+                <p className="text-center text-sm text-gray-500 mt-2">
+                    Vous avez déjà un compte? <Link href="/login" className="text-primary hover:underline">Connectez-vous</Link>
                 </p>
             </div>
 
             <div className="hidden overflow-y-auto lg:block lg:right-0 lg:fixed lg:h-full lg:inset-0 lg:left-auto lg:w-1/3 z-0 ">
-                <Image src="/images/img-register-form.png" alt="Photo" layout="fill" objectFit="cover" />
+                <Image src="/images/register-picture.png" alt="Photo" layout="fill" objectFit="cover" />
             </div>
         </div>
     );
