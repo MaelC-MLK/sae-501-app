a5a5522b26e9ff96f04710529c27a1ffc49126a0
n1noT <n8no.ttn@gmail.com>
2024-11-26 08:36:16 +0100

    mail service

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Security\Core\Encoder\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -21,6 +22,7 @@ class UserController extends AbstractController
         $this->emailService = $emailService;
     }
 
+    // Send token for public event
     #[Route('api/user/email', name: 'app_user_create', methods: ['POST'])]
     public function createUserWithEmail(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
@@ -66,6 +68,7 @@ class UserController extends AbstractController
         return new JsonResponse(['message' => 'Utilisateur créé et email de vérification envoyé.'], 201);
     }
 
+    // Invite friend with mail
     #[Route('/api/invite', name: 'invite_friend', methods: ['POST'])]
     public function inviteFriend(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
@@ -93,6 +96,7 @@ class UserController extends AbstractController
         return new JsonResponse(['message' => 'Invitation envoyée avec succès.'], 201);
     }
 
+    // Verify email for public event
     #[Route('/api/verify-email/{token}/{id}', name: 'verify_email', methods: ['GET'])]
     public function verifyEmail(string $token, int $id, EntityManagerInterface $entityManager): JsonResponse
     {
@@ -129,5 +133,78 @@ class UserController extends AbstractController
         $entityManager->flush();
 
         return new JsonResponse(['message' => 'Utilisateur inscrit à l\'événement avec succès.'], 200);
+    }
+
+    // Send token for account creation
+    #[Route('api/user/register', name: 'app_user_register', methods: ['POST'])]
+    public function registerEmail(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'] ?? null;
+        $firstName = $data['firstName'] ?? null;
+        $lastName = $data['lastName'] ?? null;
+        $plainPassword = $data['plainPassword'] ?? null;
+
+        if (empty($email) || empty($firstName) || empty($lastName) || empty($plainPassword)) {
+            return new JsonResponse(['error' => 'Informations incomplètes'], 400);
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+
+        // Hasher le mot de passe
+        $password = $passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($password);
+
+        // Générer un token de vérification
+        $token = Uuid::v4()->toRfc4122(); // Génération de token (UUID)
+        $user->setVerificationToken($token);
+
+        // Définir la date d'expiration du token
+        $expiryDate = new \DateTime('+10 minutes');
+        $user->setTokenExpiry($expiryDate);
+
+        // Désactiver l'utilisateur par défaut
+        $user->setActive(false);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Envoyer l'email de vérification
+        try {
+            $this->emailService->sendVerificationEmail($email, $token);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Impossible d\'envoyer l\'email : ' . $e->getMessage()], 500);
+        }
+
+
+
+        return new JsonResponse(['message' => 'Utilisateur créé et email de vérification envoyé.'], 201);
+    }
+    
+    #[Route('/api/verify-email-register/{token}', name: 'verify_email_register', methods: ['GET'])]
+    public function verifyEmailRegister(string $token, int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        // Rechercher l'utilisateur par token
+        $user = $entityManager->getRepository(User::class)->findOneBy(['verificationToken' => $token]);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'Token invalide ou déjà utilisé.'], 400);
+        }
+
+        // Vérifier si le token a expiré
+        if ($user->getTokenExpiry() < new \DateTime()) {
+            return new JsonResponse(['error' => 'Token expiré.'], 400);
+        }
+
+        // Supprimer le token après l'inscription
+        $user->setVerificationToken(null);
+        $user->setTokenExpiry(null);
+        $user->setActive(true);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Utilisateur inscrit avec succès.'], 200);
     }
 }
