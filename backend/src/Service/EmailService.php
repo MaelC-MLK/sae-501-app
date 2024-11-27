c18b54f1faccae89db0597541a9a78201bf01965
n1noT <n8no.ttn@gmail.com>
2024-11-27 14:13:39 +0100

    udpate link

--- backend/src/Service/EmailService.php
+++ backend/src/Service/EmailService.php
@@ -37,7 +37,7 @@ class EmailService
 
     public function sendRegisterEmail(string $recipientEmail, string $token): void
     {
-        $verificationLink = sprintf('http://localhost:8090/verify-email-register/%s', $token);
+        $verificationLink = sprintf('http://localhost:8090/register/verify/%s', $token);
 
         // Rendre le template Twig
         $htmlContent = $this->twig->render('emails/verification_email.html.twig', [
