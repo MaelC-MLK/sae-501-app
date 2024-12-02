d43edfdf41a68d99c80b7a8d73a7f3de7e6cf941
n1noT <n8no.ttn@gmail.com>
2024-12-02 14:01:14 +0100

    rate-limiter

--- backend/src/EventListener/RefreshTokenMiddleware.php
+++ backend/src/EventListener/RefreshTokenMiddleware.php
@@ -5,21 +5,26 @@ namespace App\EventListener;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class RefreshTokenMiddleware
 {
     private JWTTokenManagerInterface $jwtManager;
+    private LoggerInterface $logger;
+    private Security $security;
 
-    public function __construct(JWTTokenManagerInterface $jwtManager)
+    public function __construct(JWTTokenManagerInterface $jwtManager, LoggerInterface $logger, Security $security)
     {
         $this->jwtManager = $jwtManager;
+        $this->logger = $logger;
+        $this->security = $security;
     }
 
     public function onKernelResponse(ResponseEvent $event)
     {
-        /*
         $request = $event->getRequest();
-        $user = $request->getUser();
+        $user = $this->security->getUser();
 
         if (!$user) {
             return;
@@ -31,7 +36,7 @@ class RefreshTokenMiddleware
 
         $response->headers->setCookie(
             new Cookie(
-                'refresh',
+                'eventify',
                 $token,
                 time() + 3600,
                 '/',
@@ -41,23 +46,5 @@ class RefreshTokenMiddleware
                 'Strict'
             )
         );
-        */
-
-        $request = $event->getRequest();
-        $response = $event->getResponse();
-
-        $response->headers->setCookie(
-            new Cookie(
-                'refresh',
-                'merci',
-                time() + 3600,
-                '/',
-                null,
-                true,
-                true,
-                'Strict'
-            )
-        );
-
     }
 }
