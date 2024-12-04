ed7692228d9e6dbaab4ce82928d12baaf34b418a
TomBoutin <tomboutin.tom@gmail.com>
2024-12-04 12:22:18 +0100

    loader fix

--- frontend/app/profile/calendar/page.tsx
+++ frontend/app/profile/calendar/page.tsx
@@ -67,12 +67,12 @@ export default function Page() {
                 {
                     loading ? (
                         <>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="text-center">
-          <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-primary border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"></div>
-          <p className="mt-4 text-gray-700">Chargement...</p>
-        </div>
-      </div>
+                            <div className="flex items-center justify-center min-h-screen">
+                                <div className="text-center">
+                                    <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-primary border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"></div>
+                                    <p className="mt-4 text-gray-700">Chargement...</p>
+                                </div>
+                            </div>
                         </>
                     ) : (
                         <>
@@ -80,7 +80,7 @@ export default function Page() {
                         </>
                     )
                 }
-                
+
             </div>
         </>
     );
